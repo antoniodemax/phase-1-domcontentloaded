8b46ea9bbc02bde595519df65a66eec35e685222
Anthony <anthony.onyango@student.moringaschool.com>
2024-07-07 21:22:04 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Your code goes here
+
+//add an event listener to the document
 document.addEventListener("DOMContentLoaded", function() {
+
+    //a reference is gotten to the html content with the 'text'
     const paragraph = document.getElementById('text');
+
+    //the paragraph element is set to a new string
     paragraph.textContent = 'This is really cool!';
 });
